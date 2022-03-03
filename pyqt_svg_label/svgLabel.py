18e6101b24f5e0d88e66015fb962e9944460ddc1
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-03 13:34:19 +0900

    Get relative path from main module

--- pyqt_svg_label/svgLabel.py
+++ pyqt_svg_label/svgLabel.py
@@ -1,3 +1,5 @@
+import os, inspect, sys
+
 from PyQt5.QtGui import QPainter
 from PyQt5.QtSvg import QSvgRenderer
 from PyQt5.QtWidgets import QLabel
@@ -16,5 +18,7 @@ class SvgLabel(QLabel):
         return super().paintEvent(e)
 
     def setSvgFile(self, filename: str):
+        caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(1)).filename)
+        filename = os.path.join(caller_path, filename).replace('\\', '/')
         self.__renderer = QSvgRenderer(filename)
         self.resize(self.__renderer.defaultSize())
