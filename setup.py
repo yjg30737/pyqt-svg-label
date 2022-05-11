b9ab521d3d8810bf9b725675a71201eb5e77b3b9
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-11 09:04:15 +0900

    Update setup.py to upload this package to pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-svg-label',
-    version='0.3.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt SVG icon only label',
+    description='PyQt QLabel which supports SVG icon',
     url='https://github.com/yjg30737/pyqt-svg-label.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
-        'python-get-absolute-resource-path @ git+https://git@github.com/yjg30737/python-get-absolute-resource-path.git@main'
+        'python-get-absolute-resource-path>=0.0.1'
     ]
 )
