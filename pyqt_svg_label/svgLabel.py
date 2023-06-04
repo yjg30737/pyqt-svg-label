6166045209ebd3a834d3da1de615cf4fbeeab989
yjg30737 <yjg30737@gmail.com>
2023-06-04 10:24:38 +0900

    Update svgLabel.py

--- pyqt_svg_label/svgLabel.py
+++ pyqt_svg_label/svgLabel.py
@@ -1,7 +1,8 @@
+import os
+
 from PyQt5.QtGui import QPainter
 from PyQt5.QtSvg import QSvgRenderer
 from PyQt5.QtWidgets import QLabel
-import absresgetter
 
 
 class SvgLabel(QLabel):
@@ -11,12 +12,13 @@ class SvgLabel(QLabel):
 
     def paintEvent(self, e):
         painter = QPainter(self)
-        painter.restore()
         if self.__renderer:
             self.__renderer.render(painter)
         return super().paintEvent(e)
 
     def setSvgFile(self, filename: str):
-        filename = absresgetter.getabsres(filename)
+        filename = os.path.join(os.path.dirname(__file__), filename)
         self.__renderer = QSvgRenderer(filename)
         self.resize(self.__renderer.defaultSize())
+        length = max(self.sizeHint().width(), self.sizeHint().height())
+        self.setFixedSize(length, length)
